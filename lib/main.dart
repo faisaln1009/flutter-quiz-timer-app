f94e98c4e198b718d3c56517268f4fff3762c4bc
faisaln1009 <faisaln1009@gmail.com>
2026-06-04 10:21:01 +0600

    add project files

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 100,
+              color: Colors.white,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            Container(
+              width: 100,
+              color: Colors.lightGreen,
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
