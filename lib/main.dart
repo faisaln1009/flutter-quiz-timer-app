0b534a90322878a001087aef72a5672b52709a18
Faisal Mahmud <faisaln1009@gmail.com>
2026-06-04 10:26:40 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+runApp(
+  MaterialApp(
+    home: Scaffold(
+    backgroundColor: Colors.teal,
+    body: Row(
+     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: 100,
+            color: Colors.white,
+          ),
+          Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 100,
-              color: Colors.white,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.black,
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.blue,
-                ),
-              ],
-            ),
-            Container(
-              width: 100,
-              color: Colors.lightGreen,
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
+          Container(
+          width: 100,
+          height: 100,
+          color: Colors.black,),
+          Container(
+          width: 100,
+          height: 100,
+          color: Colors.blue,),],),
+          Container(
+          width: 100,
+          color: Colors.lightGreen,),
+    ],
+  ),
+  ),
+),
+);
 }
